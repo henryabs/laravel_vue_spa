3c8771099b7d4afff04ad0111c06512d65975007
Henry Abayan <henry.abayan@gmail.com>
2021-07-01 05:49:49 +0800

    modify created_at using momentjs and filters

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,9 @@ window.Vue = require('vue').default;
 import Index from './Index.vue';
 import router from './routes';
 import VueRouter from 'vue-router';
-
+import moment from 'moment';
+import Vue from 'vue';
+Vue.filter("fromNow", value => moment(value).fromNow());
 Vue.use(VueRouter);
 const app = new Vue({
     el: '#app',
--- resources/js/bookable/ReviewList.vue
+++ resources/js/bookable/ReviewList.vue
@@ -10,7 +10,7 @@
                     <div class="col-md-6 d-flex justify-content-end">{{ review.rating }}</div>
                 </div>
                 <div class="row">
-                    <div class="col-md-12">{{ review.created_at }}</div>
+                    <div class="col-md-12">{{ review.created_at | fromNow }}</div>
                 </div>
                 <div class="row pt-4 pb-4">
                     <div class="col-md-12">
@@ -23,6 +23,7 @@
 </template>
 
 <script>
+// import moment from 'moment';
 export default {
     props:{
         bookableId: String
@@ -39,6 +40,11 @@ export default {
         .then(response => this.reviews = response.data.data)
         .then(() => (this.loading = false));
     },
+    // filters:{
+    //     fromNow(value){
+    //         return moment(value).fromNow();
+    //     }
+    // }
 
 }
 </script>
